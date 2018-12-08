410e30c6fdb06fe9f08adfb201450cbb67d3e1cc
Vinaya Krishna Pudyodu <vinaya.krishna.dev@gmail.com>
2018-12-07 21:51:54 -0800

    Initial Commit

---
+++ MovieTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieTime
+//
+//  Created by vinaya krishna on 07/12/18.
+//  Copyright © 2018 sdsuios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
